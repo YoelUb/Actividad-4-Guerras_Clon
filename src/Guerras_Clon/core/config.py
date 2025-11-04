6202e06a293224bc35436786bf0d9887e4064b18
YoelUb <yurqubar@myuax.com>
2025-11-04 09:25:09 +0100

    Hechos test Unitarios, falta README.md

--- src/Guerras_Clon/core/config.py
+++ src/Guerras_Clon/core/config.py
@@ -1,16 +1,12 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import computed_field
-import os
-
-
-env_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), '.env')
-
 
 class Settings(BaseSettings):
-
-
-    model_config = SettingsConfigDict(env_file=env_path, extra='ignore')
-
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
 
     DB_USER: str
     DB_PASSWORD: str
